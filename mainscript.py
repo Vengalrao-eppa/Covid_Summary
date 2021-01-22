f2893b178d47ade76d387e1c2d1db2aca4afa5e1
vengalrao e <vengal.eppa@boatyardx.com>
2021-01-22 03:56:22 +0000

    updated

--- mainscript.py
+++ mainscript.py
@@ -22,12 +22,12 @@ api = Cov19API(
     structure=cases_and_deaths,
 )
 
-# def job():
-#     api.get_csv(save_as="data.csv")
+def job():
+    api.get_csv(save_as="data.csv")
     
-# # schedule.every().day.at("10:30").do(job)
-# # schedule.every().minute.at(":17").do(job)
+schedule.every().day.at("10:30").do(job)
+# schedule.every().minute.at(":17").do(job)
 # schedule.every(5).to(10).seconds.do(job)
-# while True:
-#     schedule.run_pending()
-#     time.sleep(1)
+while True:
+    schedule.run_pending()
+    time.sleep(1)
